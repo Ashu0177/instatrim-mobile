ac2a50b033a6ef8843b48108ef9c34dc9d86dccb
Ashu0177 <karthimkalaiselvan@gmail.com>
2025-09-01 14:29:35 +0530

    web: add React entry point script

---
+++ web/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const App = () => <h1>🚀 Hello from Instatrim Web</h1>;
+
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
